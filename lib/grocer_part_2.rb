31ae903bb6a35a89acdc03ffbc1cee831260d46b
james wu <jwu_nyc@yahoo.com>
2020-07-06 13:19:50 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -17,16 +17,11 @@ cart =[
 ]
 
 cart2 =[
-  {:item => "AVOCADO", :price => 3.00, :clearance => true},
-  {:item => "KALE", :price => 3.00, :clearance => false},
-  {:item => "BLACK_BEANS", :price => 2.50, :clearance => false},
-  {:item => "ALMONDS", :price => 9.00, :clearance => false},
-  {:item => "TEMPEH", :price => 3.00, :clearance => true},
-  {:item => "CHEESE", :price => 6.50, :clearance => false},
   {:item => "BEER", :price => 13.00, :clearance => false},
-  {:item => "PEANUTBUTTER", :price => 3.00, :clearance => true},
+  {:item => "BEER", :price => 13.00, :clearance => false},
+  {:item => "BEER", :price => 13.00, :clearance => false},
   {:item => "BEETS", :price => 2.50, :clearance => false},
-  {:item => "SOY MILK", :price => 4.50, :clearance => true}
+
 ]
 
 def apply_coupons(cart, coupons)
